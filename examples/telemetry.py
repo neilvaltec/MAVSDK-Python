d49aec38ba7d8bac831eb2bbb9f19669728bdccc
Neil Chou <neil@valtec.io>
2023-11-07 23:21:02 +0800

    dev: update demo telemetry.py

--- examples/telemetry.py
+++ examples/telemetry.py
@@ -7,24 +7,22 @@ import sys
 async def run():
     # Init the drone
     drone = System()
-    await drone.start("udp://:14551")
-    
-    # Start the tasks
-    # asyncio.ensure_future(print_battery(drone))
-    # asyncio.ensure_future(print_gps_info(drone))
+    await drone.start()
+
+    succeed = await drone.core.add_new_connection("udp://:14551")
+    print("first connection: ", succeed)
     asyncio.ensure_future(print_in_air(drone, 1))
     asyncio.ensure_future(print_position(drone, 1))
 
     succeed = await drone.core.add_new_connection("udp://:14552")
     print("second connection: ", succeed)
-
     asyncio.ensure_future(print_in_air(drone, 2))
     asyncio.ensure_future(print_position(drone, 2))
 
-    await drone.action.arm(int(sys.argv[1]))
-    await drone.action.takeoff(int(sys.argv[1]))
+    await drone.action.arm(1)
+    await drone.action.takeoff(1)
     await asyncio.sleep(10)
-    await drone.action.land(int(sys.argv[1]))
+    await drone.action.land(1)
 
     await drone.action.arm(2)
     await drone.action.takeoff(2)
@@ -33,16 +31,6 @@ async def run():
 
     while True:
         await asyncio.sleep(1)
-
-
-async def print_battery(drone):
-    async for battery in drone.telemetry.battery(int(sys.argv[1])):
-        print(f"Battery: {battery.remaining_percent}")
-
-async def print_gps_info(drone):
-    async for gps_info in drone.telemetry.gps_info(int(sys.argv[1])):
-        print(f"GPS info: {gps_info}")
-
 
 async def print_in_air(drone, id):
     async for in_air in drone.telemetry.in_air(id):
